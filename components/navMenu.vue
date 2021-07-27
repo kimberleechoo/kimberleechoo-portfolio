b5fc758ae8447af2c5ccddad941c7c0ebbd97cc2
Kimberlee Choo <kimberlee.choo@growthops.asia>
2021-07-27 17:00:46 +0800

    added bootstrap-vue & some testing

---
+++ components/navMenu.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav class="nav">
+        <div class="logo">
+             <NuxtLink to = "/index"><h1>KC.</h1></NuxtLink>
+        
+        </div>
+    </nav>
+</template>
+
+<script>
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,19 @@
 <template>
-    <div>
-       <NuxtLink to = "/about"><h1>about</h1></NuxtLink>
-    </div>
+    <!-- <div>
+       <NuxtLink to = "/about"><h1>cvv</h1></NuxtLink>
+    </div> -->
+   <main id = "firstScreen">
+       <section class="banner">
+           <div class="bg" style = "background-image:url('~/assets/images/landingbg.png')"></div>
+           <div class="bg">
+              <h1>KC.</h1>
+              <p>hello</p>
+           </div>
+       </section>
+   </main>
 </template>
 
 <script>
 
 </script>
+
